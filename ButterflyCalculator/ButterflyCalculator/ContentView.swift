b26f2da8bafb2c6194c2c124a016c00c8831e2c9
Angela <angela.zl.mao@gmail.com>
2019-10-21 12:10:36 +1100

    Added base project and workspace with configuration details in README file.

---
+++ ButterflyCalculator/ButterflyCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButterflyCalculator
+//
+//  Created by Angela Mao on 21/10/19.
+//  Copyright © 2019 Angela Mao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ButterflyCalculator")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ButterflyCalculator
-Interview Project for Butterfly Systems
+Interview Project for Butterfly Systems - build a simple calculator with arithmetic and clean UI for landscape and portrait.
+
+Project Details:
+* Swift 5.1
+* SwiftUI
+* Testing frameworks
+* No Core Data
+* Landscape & Portrait
+* iPad (last 2 gen per device) + iPad Pro + iPhones (6th Gen+)
