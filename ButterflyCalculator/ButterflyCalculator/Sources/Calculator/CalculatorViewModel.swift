9616cb59aef434ae6b5663415ce552e3c5936062
Angela <angela.zl.mao@gmail.com>
2019-11-05 12:51:11 +1100

    Removed comments, overflow operators not needed for Doubles

--- ButterflyCalculator/ButterflyCalculator/Sources/Calculator/CalculatorViewModel.swift
+++ ButterflyCalculator/ButterflyCalculator/Sources/Calculator/CalculatorViewModel.swift
@@ -12,8 +12,6 @@ import Combine
 /// ViewModel that stores the current state of the CalculatorView
 class CalculatorViewModel: ObservableObject, Identifiable {
     
-    // TODO: Create a way to allow operations to change and the display will automatically update
-    // TODO: Display value should remember the last value in userDefaults when app start up
     @Published var displayValue = "0"
     /// dataSource is often the model for MVVM - this model is not needed as we have no objects to instantiate
     
@@ -24,11 +22,4 @@ class CalculatorViewModel: ObservableObject, Identifiable {
     
     /// - Parameter: integer to increment
     /// - Return: display + number
-//
-//    func addition(_ number: Int) -> Int {
-//        // FIXME: Handle floating point numbers
-//        if let val = Int(displayValue)
-//
-//        return (Int(displayValue) ?? 0) &+ number
-//    }
 }
